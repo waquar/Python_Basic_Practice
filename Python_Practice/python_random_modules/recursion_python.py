f272572640401623e6df3e66b4fa7be891bde92e
waquar <waquar.alam92@gmail.com>
2019-07-19 14:29:31 +0530

    added recursion

---
+++ Python_Practice/python_random_modules/recursion_python.py
@@ -0,0 +1,32 @@
+def factorial_iteration(num):
+
+    fac = 1
+    for i in range(num):
+        fac = fac * (i + 1)
+    return  fac
+
+def factorial_recursion(num):
+    if num == 1:
+        return 1
+    else:
+            rec = num * factorial_recursion(num -1)
+            return  rec
+
+
+def fibonacci_num(num):
+    if num == 1:
+        return 0
+    if num == 2:
+        return  1
+    else:
+        return  fibonacci_num(num-1) + fibonacci_num(num-2)
+
+print("enter number")
+num = int(input())
+#print(factorial_iteration(num))
+c = factorial_recursion(num)
+print(c)
+
+#print(fibonacci_num(num))
+
+
