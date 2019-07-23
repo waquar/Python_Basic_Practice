07ee1c3289e9ecc129d6f47bbc9221c38bd06c2e
waquar <waquar.alam92@gmail.com>
2019-07-23 13:48:53 +0530

    added join

---
+++ Python_Practice/python_random_modules/usingJoin.py
@@ -0,0 +1,10 @@
+fruitlist = ['apple', 'banana', 'chikoo', 'dates', 'enjir', 'falooda']
+veg = ['potato', 'tomato', 'brinjal', 'chilly', 'cauliflower', 'spinache']
+
+a = ' and '.join(fruitlist)
+b = ' and '.join(veg)
+print(a+'\n'+b)
+
+
+#it joins the list with given value
+#joins take only one argument
