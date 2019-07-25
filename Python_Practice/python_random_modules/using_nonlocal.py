4561dc0405a227084523dd964b18bd5ecc599fac
waquar <waquar.alam92@gmail.com>
2019-07-25 15:32:30 +0530

    checking git

---
+++ Python_Practice/python_random_modules/using_nonlocal.py
@@ -0,0 +1,13 @@
+class Room(object):
+    def Employee(self, name, age):
+        self.name = name
+        self.age = age
+        comission = 200
+        def moreEmployee(self):
+            age = input()
+            nonlocal comission
+            return age+comission
+
+
+
+
