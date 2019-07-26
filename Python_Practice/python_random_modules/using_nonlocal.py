ef7aa73ad27a95450427c35a395a452dc2f93ff2
waquar <waquar.alam92@gmail.com>
2019-07-26 14:49:32 +0530

    reading csvfiles

--- Python_Practice/python_random_modules/using_nonlocal.py
+++ Python_Practice/python_random_modules/using_nonlocal.py
@@ -1,8 +1,10 @@
+
 class Room(object):
     def Employee(self, name, age):
+        comission = 200
         self.name = name
         self.age = age
-        comission = 200
+
         def moreEmployee(self):
             age = input()
             nonlocal comission
