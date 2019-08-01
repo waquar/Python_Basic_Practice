d51d828690a7454d1d855d2c928c40c6fbd73f9f
waquar <waquar.alam92@gmail.com>
2019-08-01 16:22:58 +0530

    added regex exercise

--- Python_Practice/python_random_modules/regular_expressions.py
+++ Python_Practice/python_random_modules/regular_expressions.py
@@ -1,5 +1,32 @@
 import re
 
 #findall, search, split, sub, finditer
+mystr = '''Tata Limited
+Dr. David Landsman, executive director
+18, Grosvenor Place
+London SW1X 7HSc
+Phone: +44 (20) 7235 8281
+Fax: +44 (20) 7235 8727
+Email: tata@tata.co.uk
+Website: www.europe.tata.com
+Directions: View map
+Tata Sons, North America
+1700 North Moore St, Suite 1520
+Arlington, VA 22209-1911
+USA
+Phone: +1 (703) 243 9787
+Fax: +1 (703) 243 9791
+66-66
+455-4545
+Email: northamerica@tata.com 
+Website: www.northamerica.tata.com
+Directions: View map fass
+harry bhai lekin
+bahut hi badia aadmi haiaiinaiiiiiiiiiiii'''
+
+stringreg = re.compile(r'David')
+matchfound = stringreg.finditer(mystr)
+for match in matchfound:
+    print(match)
 
 
