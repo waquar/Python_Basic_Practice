464ae330646c89bbaea45c81631c5bc05d1ca6b9
waquar <waquar.alam92@gmail.com>
2019-08-07 16:07:37 +0530

    added pickle module

---
+++ Python_Practice/python_random_modules/Using_Pickle_Module.py
@@ -0,0 +1,20 @@
+import pickle
+
+# pickle used for storing data as a file and can be reused later
+#---------------------------------------------------------------------------
+# making a pickle file
+
+list1 = ['apple' , 'grapes', 'banana', 'muskmelon' , 'watermelon']
+file = 'fruits.pkl'
+file_fruit = open(file, 'wb')
+pickle.dump(list1, file_fruit)            #used for storing the pickle file
+file_fruit.close()
+
+#---------------------------------------------------------------------------
+# reading a pickle file
+
+file = 'fruits.pkl'
+file_fruit = open(file, 'rb')
+myfruits = pickle.load(file_fruit)                      #used for loading the pickle file
+print(myfruits)
+print(type(myfruits))
