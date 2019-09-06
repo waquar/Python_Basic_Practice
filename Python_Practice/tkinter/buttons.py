fbcc7f9e11bab085a1ba8965cb6f58d23d42eb9e
waquar <waquar.alam92@gmail.com>
2019-09-06 14:11:10 +0530

    update in tkinter buttons

---
+++ Python_Practice/tkinter/buttons.py
@@ -0,0 +1,17 @@
+from tkinter import  *
+root = Tk()
+root.geometry("789x567")
+frame = Frame(root, borderwidth = 6, bg = "grey", relief = SUNKEN)
+frame.pack(side =LEFT, anchor = "nw")
+
+def hello():
+    print("hello waquar")
+
+b1 = Button(frame, fg = "red", text="waquar")
+b1.pack(side = LEFT)
+b2 = Button(frame, fg = "red", text="alam")
+b2.pack(side = RIGHT)
+b3 = Button(frame, fg = "red", text="ansari", command = hello)             #only give function name, dont call
+b3.pack()
+
+root.mainloop()
